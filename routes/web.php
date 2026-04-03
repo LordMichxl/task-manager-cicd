908376c2bfce7d11d62a46d56f8c45358f6001a9
jarrah-web <diengmamediarra372@gmail.com>
2026-04-03 23:03:32 +0000

    feat(tasks):ajouter les routes d’édition et de mise à jour des tâches

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/tasks/{task}/edit', [TaskController::class , 'edit'])->name('tasks.edit');
+Route::put('/tasks/{task}', [TaskController::class , 'update'])->name('tasks.update');
